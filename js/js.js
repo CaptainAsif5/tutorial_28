3f780b9c75a4ba589d7fd6c6f0a955c95273faeb
asif khan <asifkhandeveloper4@gmail.com>
2023-09-17 18:15:23 +0500

    iamge hover to change image and spotify landing page responsive

---
+++ js/js.js
@@ -0,0 +1,76 @@
+var pen = [
+    {
+        id: 1, name: "Song 1", image: "../images/1.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 2, name: "Song 2", image: "../images/2.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 3, name: "Song 3", image: "../images/3.jpg", description: "Title, Text on the card"
+    },
+    {
+        id: 4, name: "Song 4", image: "../images/4.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 5, name: "Song 5", image: "../images/5.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 6, name: "Song 6", image: "../images/1.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 7, name: "Song 7", image: "../images/2.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 8, name: "Song 8", image: "../images/3.jpg", description: "Title, Text on the card"
+    },
+    {
+        id: 9, name: "Song 9", image: "../images/4.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 10, name: "Song 10", image: "../images/5.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 11, name: "Song 11", image: "../images/4.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 12, name: "Song 12 ", image: "../images/5.jpg",description: "Title, Text on the card"
+    }
+]
+var sports = [
+    {
+        id: 1, name: "sports 1", image: "../images/1.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 2, name: "sports 2", image: "../images/2.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 3, name: "sports 3", image: "../images/3.jpg", description: "Title, Text on the card"
+    },
+    {
+        id: 4, name: "sports 4", image: "../images/4.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 5, name: "sports 5", image: "../images/5.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 6, name: "sports 6", image: "../images/1.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 7, name: "sports 7", image: "../images/2.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 8, name: "sports 8", image: "../images/3.jpg", description: "Title, Text on the card"
+    },
+    {
+        id: 9, name: "sports 9", image: "../images/4.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 10, name: "sports 10", image: "../images/5.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 11, name: "sports 11", image: "../images/4.jpg",description: "Title, Text on the card"
+    },
+    {
+        id: 12, name: "sports 12 ", image: "../images/5.jpg",description: "Title, Text on the card"
+    }
+]
